02765433af725f9a751c6a2a03cd17064075e86b
LukeBooker <lukebooker08@gmail.com>
2022-03-08 14:27:06 +0000

    add topic buttons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,50 @@
 const Header = () => {
   return (
-    <section class="section mt-6 bg-img">
-      <p class="title is-1 has-text-light mt-4">listen as well as you hear</p>
-    </section>
+    <div>
+      <section class="section mt-6 bg-img">
+        <p class="title is-1 has-text-light mt-4">listen as well as you hear</p>
+      </section>
+      <div>
+        <section class="box mb-5 has-background-info-light" id="topic-box">
+          <h3 class="has-text-grey-darker has-text-weight-semibold">
+            Your daily topics
+          </h3>
+          <div>
+            {" "}
+            <button
+              id="topic-button"
+              class="button is-info is-outlined is-rounded  mx-2 my-2"
+            >
+              Rounded
+            </button>
+            <button
+              id="topic-button"
+              class="button is-info is-outlined is-rounded  mx-2 my-2"
+            >
+              Rounded
+            </button>
+            <button
+              id="topic-button"
+              class="button is-info is-outlined is-rounded  mx-2 my-2"
+            >
+              Rounded
+            </button>
+            <button
+              id="topic-button"
+              class="button is-info is-outlined is-rounded  mx-2 my-2"
+            >
+              Rounded
+            </button>
+            <button
+              id="topic-button"
+              class="button is-info is-outlined is-rounded  mx-2 my-2"
+            >
+              Rounded
+            </button>
+          </div>
+        </section>
+      </div>
+    </div>
   );
 };
 
