13822b6ef4b387735558fda86b2f690241329677
LukeBooker <lukebooker08@gmail.com>
2022-03-09 10:24:17 +0000

    refactor html tags for consistency

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,21 @@
 const Header = ({ topic }) => {
   if (!topic) {
     return (
-      <section>
+      <>
         <header className="section mt-6 bg-img-home">
           <p className="title is-1 has-text-light mt-4">
             a little place for civil discourse
           </p>
         </header>
-      </section>
+      </>
     );
   } else {
     return (
-      <section>
+      <>
         <header className="section mt-6 bg-img-topics">
           <p className="title is-1 has-text-white mt-4">{topic}</p>
         </header>
-      </section>
+      </>
     );
   }
 };
